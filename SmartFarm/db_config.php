ff5a26248fd9582baa832e607bc36da876e1029c
Alvin-sserunkuuma <133165842+Alvin-sserunkuuma@users.noreply.github.com>
2026-05-06 21:17:16 +0300

    Enable default fetch mode for PDO to fetch associative arrays

--- SmartFarm/db_config.php
+++ SmartFarm/db_config.php
@@ -9,7 +9,7 @@ $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Shows errors clearly
-    // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // Returns data as arrays
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // Returns data as arrays
     PDO::ATTR_EMULATE_PREPARES   => false,                  // Security feature
 ];
 
